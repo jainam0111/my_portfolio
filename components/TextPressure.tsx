3460c45f3828665379d7d60ae454965c53bb1325
Jainam Bhavsar <jainam.bhavsar0111@gmail.com>
2025-12-01 12:45:01 +0530

    Fix TypeScript build error in TextPressure component - ref callback now returns void

--- components/TextPressure.tsx
+++ components/TextPressure.tsx
@@ -215,7 +215,9 @@ const TextPressure = ({
         {chars.map((char, i) => (
           <span
             key={i}
-            ref={el => (spansRef.current[i] = el)}
+            ref={el => {
+              spansRef.current[i] = el
+            }}
             data-char={char}
             style={{
               display: 'inline-block',
